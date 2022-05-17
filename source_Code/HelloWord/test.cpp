ae56ec43bbcb2ca4dcd4b895b7c6fc83c38b86b0
841590161 <841590161@qq.com>
2022-05-17 10:31:42 +0800

    无

--- source_Code/HelloWord/test.cpp
+++ source_Code/HelloWord/test.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main()
 {
-	cout << "hello word\n" << endl;
+	//vector<int> a;
+
+	//for (int i = 0; i < 10; i++) {
+	//	a.push_back(i); // 向数组a中添加元素
+	//}
+	//for (int i = 0; i < 10; i++) {
+	//	cout<< a.at(i) << " "; // 打印出 a 数组中的元素
+	//}
+	//cout << endl;
+	//cout << "hello word\n" << endl;
+	printf("hello wordl\n");
+
 	return 0;
 }
